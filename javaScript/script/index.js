c68b3248bf1ab48d2e6dba75695b63185cae5ac7
nazmul <md.nazmulislam787@gmail.com>
2020-09-20 16:47:25 +0300

    if else practice

--- javaScript/script/index.js
+++ javaScript/script/index.js
@@ -1,110 +1,18 @@
-// const arrayObject=[
-//     'potato',30,3,true
-// ];
-// console.log(arrayObject[0]);
-// console.log(arrayObject[3]);
+const productPrice = 9;
 
-const productObjInfo =
-{
-    productName: 'potato',
-    productPrice: 30,
-    productQuantity: 3,
-    productAbailabe: true
+let freeShipping = false;
+let handPicked = false;
+
+if (productPrice >= 100) {
+    freeShipping = true;
+    handPicked = false
+} else if (productPrice >= 60) {
+    freeShipping = false;
+    handPicked = true
 }
-const productObjInfo2 =
-{
-    productName: 'alo',
-    productPrice: 30,
-    productQuantity: 3,
-    productAbailabe: true
+else {
+    freeShipping = false;
+    handPicked = true;
 }
-const productObjInfo3 =
-{
-    productName: 'onion',
-    productPrice: 30,
-    productQuantity: 3,
-    productAbailabe: true
-}
-
-//object data distucturing 
-
-// const {productName,productPrice}=productObjInfo3;
-// console.log(productName);
-
-
-function showProductInformation(reciveFromObject,productArgument='Default value') {
-    //here is data distucturing 
-   const {productName,productPrice,productQuantity,productAbailabe} =  reciveFromObject;
-//when we deploy distucturing don't need keep name parameter for show the result.
-return `${productArgument} product name is : ${productName} product weight is : ${productPrice} product quntity is : ${productQuantity} product ableable :${productAbailabe}`;
-
-
-}
-// showProductInformation(productObjInfo);
-// showProductInformation(productObjInfo2);
-
-const recive1 =showProductInformation(productObjInfo3 ,'productArgument1');
-const recive2 =showProductInformation(productObjInfo2);
-const recive3 =showProductInformation(productObjInfo,'productArgument3');
-console.log(recive1);
-console.log(recive2);
-console.log(recive3);
-
-
-//here is array distucturing
-const productArray= ['followe',30,3,true];
-const[name,price,quntity,abailalbe]=productArray;
-console.log(name);
-
-//for loop apply here
-
-const student=[
-    {
-        name:'Nazmul',
-        id:100,
-        department:"eee"
-    },
-    {
-        name:'Akbar',
-        id:101,
-        department:'eee'
-    },
-    {
-        name:'ali',
-        id:102,
-        department:'mp'
-    },
-    {
-        name:'Abdullah',
-        id:103,
-        department:'cse'
-    },
-];
-
-
-
-function studentInformation(student){
-    let singleStudent='';
-    
-    for (let index = 0; index < student.length; index++) {
-        
-        singleStudent += `
-        name -${student[index].name}
-        id -${student[index].id}
-        depatment -${student[index].department}
-
-        ` ;
-        
-    }
-    return singleStudent;
-    
-};
-
-const reciveStudentInfo1=studentInformation(student);
-
-console.log(reciveStudentInfo1);
-
-let num1=1;
-num1+=1;
-console.log(num1)
-
+console.log(freeShipping)
+console.log(handPicked)
--- javaScript/script/practice.js
+++ javaScript/script/practice.js
@@ -1,50 +1,7 @@
-const student=[
-    {
-        name:'Nazmul',
-        id:100,
-        department:"eee"
-    },
-    {
-        name:'Akbar',
-        id:101,
-        department:'eee'
-    },
-    {
-        name:'ali',
-        id:102,
-        department:'mp'
-    },
-    {
-        name:'Abdullah',
-        id:103,
-        department:'cse'
-    },
-];
+const num1 = '3';
+const num2 = 3;
+console.log(num1 <= num2);
+console.log(num1 !== num2 );
 
 
-
-function studentInformation(student){
-    let singleStudent='';
-    
-    for (let index = 0; index < student.length; index++) {
-        
-        singleStudent += `
-        name -${student[index].name}
-        id -${student[index].id}
-        depatment -${student[index].department}
-
-        ` ;
-        
-    }
-    return singleStudent;
-    
-};
-
-const reciveStudentInfo1=studentInformation(student);
-
-console.log(reciveStudentInfo1);
-
-let num1=1;
-num1+=1;
-console.log(num1)
 
